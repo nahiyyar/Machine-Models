364724dd86645e79d28066ffeb8a6bbc5fba81c0
nahiyyar <naiyyarmodan34@gmail.com>
2025-11-27 11:26:27 +0530

    commit -1

--- params.py
+++ params.py
@@ -1,15 +1,14 @@
 import numpy as np
 
-    
 def sigmoid(self,z):
-        return 1/(1+np.exp(-z))
+    return 1/(1+np.exp(-z))
     
-def reul(self,z):
-        return max(0,z)
+def relu(self,z):
+    return max(0,z)
     
 def tanh(self,z):
-        return (np.exp(z)-np.exp(-z))/(np.exp(z)+np.exp(-z))
+    return (np.exp(z)-np.exp(-z))/(np.exp(z)+np.exp(-z))
     
 def softmax(self,z):
-        return np.exp(z)/np.sum(np.exp(z))
+    return np.exp(z)/np.sum(np.exp(z))
     
